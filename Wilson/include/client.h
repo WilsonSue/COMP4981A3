7de6bf8f2d7c3c8cb79caeea855bcd9d4b3fcfc5
WilsonSue <113160128+WilsonSue@users.noreply.github.com>
2024-04-11 20:22:38 -0700

    Update client.h

--- Wilson/include/client.h
+++ Wilson/include/client.h
@@ -1,31 +1,40 @@
-#ifndef COMP4981_ASS3_CLIENT_H
-#define COMP4981_ASS3_CLIENT_H
+#ifndef
+#define COMP4981_A3_SERVER_H
 
 #include <arpa/inet.h>
-#include <errno.h>
-#include <ncurses.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <sys/select.h>
-#define MAXLINE 1024
-#define TENNER 10
-#define TWENNER 20
-#define TWENNYSEVEN 27
-#define MAX_PLAYERS 27
-#define MAX_WIDTH 80
-#define MAX_HEIGHT 24
 
+#define MAXLINE 64
+#define MAX_PLAYERS 4
+#define TENNER 10
+
+// Player structure
 typedef struct
 {
     int x;
     int y;
+    int occupied;
 } Player;
 
-void update_positions(const char *message, Player *players, int *num_players);
-void draw_players(Player *players, int num_players);
+// Structure to hold client information
+typedef struct
+{
+    struct sockaddr_in addr;
+    int                player_id;
+} Client;
 
-#endif    // COMP4981_ASS3_CLIENT_H
+static Client clients[MAX_PLAYERS];    // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+static int    num_clients = 0;         // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+
+// Function declarations
+void update_position(Player *players, int player_id, int dx, int dy);
+int  assign_position(Player *players);
+int  find_or_assign_client(struct sockaddr_in *cliaddr, Player *players);
+void broadcast_positions(int sockfd, Player *players);
+
+#endif
